33d633c8538f2ed958e383de02d0017ebeb9ddd5
Janek Brandt <brandt.janek@gmail.com>
2017-03-24 08:54:38 -0700

    add methods and tests to team class to get all current teams, clear list of teams and get a specific team by it's name

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,13 +1,32 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Team {
   private String mName;
+
+  private static Map<String, Team> instances = new HashMap<String, Team>();
 
   public Team(String name) {
     mName = name;
 
+    instances.put(name, this);
   }
 
   public String getName() {
     return mName;
+  }
+
+
+  public static Map<String, Team> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public static Team getTeam(String name) {
+    return instances.get(name);
   }
 
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -2,7 +2,6 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TeamTest {
-
   private Team testTeam;
 
   @Before
@@ -14,17 +13,34 @@ public class TeamTest {
   public void TearDown() {
     testTeam = null;
     assertNull(testTeam);
+    Team.clear();
   }
 
   @Test
   public void team_instantiatesCorrectly() {
-    // Team testTeam = new Team("team awesome");
     assertTrue(testTeam instanceof Team);
   }
 
   @Test
   public void getName_returnsTeamNameCorrectly_String() {
     assertEquals("team awesome", testTeam.getName());
+  }
+
+  @Test
+  public void all_instanceIsIncludedInMap() {
+    assertTrue(Team.all().containsValue(testTeam));
+  }
+
+  @Test
+  public void clear_resetsInstancesCorrectly() {
+    assertEquals(1, Team.all().size());
+    Team.clear();
+    assertEquals(0, Team.all().size());
+  }
+
+  @Test
+  public void getTeam_returnsCorrectTeam() {
+    assertEquals(testTeam, Team.getTeam("team awesome"));
   }
 
 }
